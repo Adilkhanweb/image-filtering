6326174d4c7451979738df27cbeedacae6b26d74
Adilkhan Kuchkarov <adilkhan.nurlanuly@gmail.com>
2022-10-23 19:02:51 +0600

    test

--- image_filtering_app2/settings.py
+++ image_filtering_app2/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [BASE_DIR / 'static', ]
-STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
